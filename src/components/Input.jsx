80915bed2dc03105037065808ba99c11cac57765
Sviatoslav Yezhelyi <svyatoslav.fe@gmail.com>
2024-04-15 18:46:04 +0300

    Add New Component and Component list

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,4 +1,6 @@
-export default function Input({ label, textarea, ...props }) {
+import { forwardRef } from "react";
+
+const Input = forwardRef(function Input({ label, textarea, ...props }, ref) {
   const classes = "w-full p-1 border-b-2 rounded-sm border-sone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600";
   
     return (
@@ -6,7 +8,9 @@ export default function Input({ label, textarea, ...props }) {
       <label className="text-sm font-bold uppercase text-stone-500" htmlFor="">
         {label}
       </label>
-      {textarea ? <textarea className={classes} {...props}></textarea> : <input className={classes} {...props} />}
+      {textarea ? <textarea ref={ref} className={classes} {...props}></textarea> : <input ref={ref} className={classes} {...props} />}
     </p>
   );
-}
+})
+
+export default Input;
