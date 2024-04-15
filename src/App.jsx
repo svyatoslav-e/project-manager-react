89443a9a03341f9e1c45a845bd7a7b7d6e0eebae
Sviatoslav Yezhelyi <svyatoslav.fe@gmail.com>
2024-04-15 18:19:55 +0300

    new project form toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
+import { useState } from 'react';
+
 import SideBar from "./components/SideBar";
+import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleStartAddProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null
+      }
+    })
+  }
+
+  let content;
+
+  if(projectsState.selectedProjectId === null) {
+    content = <NewProject />
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <SideBar />
-      <NoProjectSelected />
+      <SideBar onStartAddProject={handleStartAddProject}/>
+      {content}
     </main>
   );
 }
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,11 +1,11 @@
 import Button from "./Button";
 
-export default function SideBar() {
+export default function SideBar({onStartAddProject}) {
   return ( 
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
       <h2 className="mb-8 font-bold uppercase md:text-sl text-stone-200">My Projects</h2>
       <div>
-        <Button> 
+        <Button onClick={onStartAddProject}> 
            + Add Project
         </Button>
       </div> 
